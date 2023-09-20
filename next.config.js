7daf5c0c370620957d9a37909a50683fd8221e66
Sarvesh-Ghildiyal <sarveshghildiyal@gmail.com>
2023-09-20 22:47:23 +0530

    nex.config.js changed third time

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  // Enabling React's Strict Mode
+const nextConfig = {
   reactStrictMode: true,
-
-  // Using SWC for minification
   swcMinify: true,
+}
 
-  // Setting the base path and asset prefix for production deployment on GitHub Pages
-  basePath: isProd ? '/Chainlink-Dehradun.github.io' : '',
-  assetPrefix: isProd ? '/Chainlink-Dehradun.github.io/' : '',
-};
+module.exports = nextConfig
