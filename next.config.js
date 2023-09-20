581bc460535a5c749de3ebb06259caa7eb01c057
Sarvesh-Ghildiyal <sarveshghildiyal@gmail.com>
2023-09-20 22:19:55 +0530

    nex.config.js changed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: isProd ? '/Chainlink-Dehradun.github.io' : '',
-  assetPrefix: isProd ? '/Chainlink-Dehradun.github.io/' : '',
+  // basePath: isProd ? '/Chainlink-Dehradun.github.io' : '',
+  // assetPrefix: isProd ? '/Chainlink-Dehradun.github.io/' : '',
 }
 
 module.exports = nextConfig
