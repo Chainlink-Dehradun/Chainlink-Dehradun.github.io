4fd1608f46b16271e629501c52d098ebc30295c4
Sarvesh-Ghildiyal <sarveshghildiyal@gmail.com>
2023-09-20 23:01:52 +0530

    link trial 1

--- components/shared/Purpulebutton.jsx
+++ components/shared/Purpulebutton.jsx
@@ -1,20 +1,21 @@
 const Button = ({ title }) => {
   return (
-   <a href="https://chat.whatsapp.com/FF3Vz1PeiJACST7WNrACNI"
-   target="_blank" rel="noopener noreferrer">
-     <button
-      disabled={false}
-      className="z-2"
+    <a 
+      href="https://chat.whatsapp.com/FF3Vz1PeiJACST7WNrACNI" 
+      target="_blank" 
+      rel="noopener noreferrer"
+      className="z-2 inline-block"
+      style={{
+        background: "url('/hero/purpule_button.svg') no-repeat center",
+        backgroundSize: "contain",
+      }}
     >
-      <div className="bg-[url('/hero/purpule_button.svg')] bg-no-repeat bg-center bg-contain z-10">
-        <div className=" place-content-center flex justify-center">
-          <div className="h-[90px] md:h-[110px] 2xl:h-[200px] mx-[40px] md:mx-[40px] 2xl:mx-[60px] sm:font-medium text-qiskit-white text-[17px]  2xl:text-[24px] pt-[30px] md:pt-[40px] 2xl:pt-[80px] sm:pt-[30px]">
-            {title}
-          </div>
+      <div className="place-content-center flex justify-center">
+        <div className="h-[90px] md:h-[110px] 2xl:h-[200px] mx-[40px] md:mx-[40px] 2xl:mx-[60px] sm:font-medium text-qiskit-white text-[17px] 2xl:text-[24px] pt-[30px] md:pt-[40px] 2xl:pt-[80px] sm:pt-[30px]">
+          {title}
         </div>
       </div>
-    </button>
-   </a>
+    </a>
   );
 };
 
